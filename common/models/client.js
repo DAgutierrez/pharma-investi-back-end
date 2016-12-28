0173778c6e0cb8ed7a521b2a3cb8cc5758d3616f
diego gutierrez <diego@MacBook-Pro-de-diego.local>
2016-12-28 07:49:59 -0300

    new changes

--- common/models/client.js
+++ common/models/client.js
@@ -1,5 +1,11 @@
 'use strict';
+//
+// var user = require('./client.json');
+
+import user from './client.json';
 
 module.exports = function(Client) {
+
+  console.log(user);
 
 };
--- server/boot/model-example.js
+++ server/boot/model-example.js
@@ -5,7 +5,7 @@
 
 module.exports = function(app) {
 
-  var Client = app.models.client;
+  var Client = app.models.Client;
   var Role = app.models.Role;
   var RoleMapping = app.models.RoleMapping;
   var Team = app.models.Team;
@@ -19,54 +19,9 @@ module.exports = function(app) {
     console.log('migrate!');
 
     Client.create([
-        {username: 'John', email: 'john@doe.com', password: 'opensesame'},
-        {username: 'Jane', email: 'jane@doe.com', password: 'opensesame'},
-        {username: 'Bob', email: 'bob@projects.com', password: 'opensesame'}
-      ], function(err, users) {
-
-        if (err) throw err;
-
-        console.log('Created users:', users);
-
-        // create project 1 and make john the owner
-        users[0].projects.create({
-          name: 'project1',
-          balance: 100
-        }, function(err, project) {
-          if (err) throw err;
-
-          console.log('Created project:', project);
-
-          // add team members
-          Team.create([
-            {ownerId: project.ownerId, memberId: users[0].id},
-            {ownerId: project.ownerId, memberId: users[1].id}
-          ], function(err, team) {
-            if (err) throw err;
-
-            console.log('Created team:', team);
-          });
-        });
-
-        //create project 2 and make jane the owner
-        users[1].projects.create({
-          name: 'project2',
-          balance: 100
-        }, function(err, project) {
-          if (err) throw err;
-
-          console.log('Created project:', project);
-
-          //add team members
-          Team.create({
-            ownerId: project.ownerId,
-            memberId: users[1].id
-          }, function(err, team) {
-            if (err) throw err;
-
-            console.log('Created team:', team);
-          });
-        });
+        {username: 'admin', email: 'admin@gmail.com', password: 'admin'},
+      ], function(err, clients) {
+        if (err) console.log(err.stack);
 
         //create the admin role
         Role.create({
@@ -74,24 +29,15 @@ module.exports = function(app) {
         }, function(err, role) {
           if (err) throw err;
 
-          console.log('Created role:', role);
-
           //make bob an admin
           role.principals.create({
             principalType: RoleMapping.USER,
-            principalId: users[2].id
+            principalId: clients[0].id
           }, function(err, principal) {
             if (err) throw err;
 
-            console.log('Created principal:', principal);
           });
         });
       });
-
-
   })
-
-
-
-
 };
