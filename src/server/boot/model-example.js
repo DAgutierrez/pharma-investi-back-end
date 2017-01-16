71c5f5291b00a1fdd51f8207bcfff9b6cacae012
diego gutierrez <diego@MacBook-Pro-de-diego.local>
2017-01-16 11:44:19 -0300

    adding equipment Model

--- src/server/boot/model-example.js
+++ src/server/boot/model-example.js
@@ -2,39 +2,37 @@
  * Created by diego on 27-12-16.
  */
 
-module.exports =  function(app) {
+module.exports =  async function(app) {
 
   // var Client = app.models.Client;
   // var Role = app.models.Role;
   // var RoleMapping = app.models.RoleMapping;
   // var Team = app.models.Team;
-  //
-  //
-  // var dataSource = app.dataSources.mysqlCorp;
-  //
-  // dataSource.automigrate(async  () => {
-  //
-  //
-  //   // let client = {username: 'admin', email: 'admin@gmail.com', password: 'admin'};
-  //   //
-  //   // let clientSave = await Client.create(client);
-  //   //
-  //   // let role = Role.create({name: 'admin'};
-  //
-  //
-  //   Client.create([
-  //     {username: 'admin', email: 'admin@gmail.com', password: 'admin'},
-  //   ], function(err, users) {
-  //     if (err) throw err;
-  //
-  //
+
+  var dataSource = app.dataSources.mysqlCorp;
+
+
+  //autoUpdate Tables
+  let update = await dataSource.autoupdate();
+
+
+  // Create Role
+  // let roleAdmin = await Role.create({name: 'admin'});
+
+
+  // if(roleAdmin) {
+  //   console.log('Created role:', roleAdmin);
+  // }
+
+
+
   //     //create the admin role
   //     Role.create({
   //       name: 'admin'
   //     }, function(err, role) {
   //       if (err) throw err;
   //
-  //       console.log('Created role:', role);
+  //
   //
   //       //make bob an admin
   //       role.principals.create({
